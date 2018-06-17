167f25da8e2122f901a80afdafd32fde10be6a24
efloden <earl.mark.27@gmail.com>
2018-06-17 20:14:54 +1000

    Updates

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './containers/App'
-import registerServiceWorker from './registerServiceWorker'
-import 'font-awesome/css/font-awesome.css'
-
-ReactDOM.render(<App />, document.getElementById('root'))
-registerServiceWorker()
