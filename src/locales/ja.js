3aa3e7109d1e319d0a7adea505b3997a97bfbef1
efloden <earl.mark.27@gmail.com>
2018-09-08 21:57:34 +0900

    chore: upgrade to react 16.5

--- src/locales/ja.js
+++ src/locales/ja.js
@@ -3,7 +3,7 @@ export const JAPANESE_STATE = {
   displayName: '日本語',
   messages: {
     'app.about.intro': 'こんにちは。',
-    'app.about.location': '東京に住んでいるウエブ開発者です。',
+    'app.about.location': '東京に住んでいるソフトエンジニアです。',
     'app.about.interest': 'オープンソースやIT技術を活用し国際化に貢献する事に対する熱意を持っています。',
     'app.header.greeting': 'アールです',
     'app.header.subtitle': 'フルスタック開発者 | オープンソース派 | 三度の飯よりプログラミング'
