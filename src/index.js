55953607757df73946edab55dde25a321785bb13
ts-earl.floden <ts-earl.floden@rakuten.com>
2018-09-21 14:19:22 +0900

    set up google analytics

--- src/index.js
+++ src/index.js
@@ -1,14 +1,42 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
 import App from './containers/App'
 import registerServiceWorker from './registerServiceWorker'
 import ConnectedIntlProvider from './ConnectedIntlProvider'
 import rootReducer from './reducers/index'
+import reduxLogger from 'redux-logger'
+// import { CHANGE_LOCALE } from './actions/action-types'
 import 'font-awesome/css/font-awesome.css'
 
-const store = createStore(rootReducer)
+// Redux Beacon --->
+import { createMiddleware } from 'redux-beacon'
+import logger from '@redux-beacon/logger'
+import GoogleAnalytics, { trackEvent } from '@redux-beacon/google-analytics'
+// <--- Redux Beacon
+
+// Redux Beacon --->
+
+const eventsMap = {
+  'CHANGE_LOCALE': trackEvent(action => ({
+    category: 'Locale',
+    action: 'change',
+    label: action.payload.locale.displayName
+  }))
+}
+
+
+
+const gaMiddleware = createMiddleware(eventsMap, GoogleAnalytics(), {
+  logger,
+})
+// <--- Redux Beacon
+
+const store = createStore(
+  rootReducer,
+  applyMiddleware(gaMiddleware, reduxLogger)
+)
 
 ReactDOM.render(
   <Provider store={store}>
