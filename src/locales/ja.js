738819061b4eca025164ff0077bb32ce99e45926
efloden <earl.mark.27@gmail.com>
2018-11-11 15:26:00 +0900

    fix typo

--- src/locales/ja.js
+++ src/locales/ja.js
@@ -6,7 +6,7 @@ export const JAPANESE_STATE = {
     'app.about.location': '東京に住んでいるソフトエンジニアです。',
     'app.about.interest': 'オープンソースやIT技術を活用し国際化に貢献する事に対する熱意を持っています。',
     'app.header.greeting': 'アールです',
-    'app.header.subtitle': 'ウエブ開発者 | オープンソース好き | ',
+    'app.header.subtitle': 'ウェブ開発者 | オープンソース派 | ',
     'app.header.sub1': '日本文化好き',
     'app.header.sub2': 'デジタルデザイナー',
     'app.header.sub3': '国際プログラマー',
